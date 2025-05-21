3faf906d6217565136417dea8f437c813f271625
SaStefhany <sarastefanycamposmacedo@gmail.com>
2025-05-21 15:46:19 -0300

    teste

--- book/urls.py
+++ book/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='finder'),
-    path('pesquisar/', views.pesquisar, name='pesquisar'),
-    path('cadastrar/', views.cadastrar, name='cadastrar'),
-    path('listagem/', views.listagem, name='listagem'),
+    # path('pesquisar/', views.pesquisar, name='pesquisar'),
+    # path('cadastrar/', views.cadastrar, name='cadastrar'),
+    # path('listagem/', views.listagem, name='listagem'),
 ]
 
 
--- book/views.py
+++ book/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'finder/home.html')
-
-def pesquisar(request):
-    return render(request, 'finder/pesquisar.html')
-
-def cadastrar(request):
-    return render(request, 'finder/cadastrar.html')
-
-def listagem(request):
-    return render(request, 'finder/listagem.html')
-
-# views.py
-def home(request):
-    return render(request, 'finder/finder.html')  
+    return render(request, 'book/index.html')  
 
 
 
