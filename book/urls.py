405320e2039ee0fda09caca924d31d6f409ba8ba
SaStefhany <sarastefanycamposmacedo@gmail.com>
2025-05-22 16:04:00 -0300

    cadastro
    sistema

--- book/urls.py
+++ book/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'finder'
+
 urlpatterns = [
-    path('', views.home, name='finder'),
-    path('pesquisar/', views.pesquisar, name='pesquisar'),
+    path('', views.home, name='home'),
     path('cadastrar/', views.cadastrar, name='cadastrar'),
+    path('pesquisar/', views.pesquisar, name='pesquisar'),
     path('listagem/', views.listagem, name='listagem'),
 ]
+
 
 
 
--- book/views.py
+++ book/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 
 def home(request):
+    """Renderiza a página inicial."""
     return render(request, 'finder/home.html')
 
 def pesquisar(request):
+    """Renderiza a página de pesquisa de livros."""
     return render(request, 'finder/pesquisar.html')
 
 def cadastrar(request):
+    """Renderiza a página de cadastro de livros."""
     return render(request, 'finder/cadastrar.html')
 
 def listagem(request):
+    """Renderiza a página de listagem de livros."""
     return render(request, 'finder/listagem.html')
 
-# views.py
 def home(request):
-    return render(request, 'finder/finder.html')  
+    return render(request, 'finder/base.html' )
+
+def home(request):
+    return render(request, 'finder/index.html' )
+  
 
 
 
